18a6d7b8f3f2b1cc067b162654c6d1598519fc1b
Nikhitha2025 <nikhitha33263@gmail.com>
2025-02-18 22:51:30 +0530

    Zomato order tracking

---
+++ Day1/Zomato.c
@@ -0,0 +1,28 @@
+Scenario 1:
+Zomato Order Tracking System answer
+#include <stdio.h>
+int main() {
+	int status;
+	while(1) {
+		printf("Enter order status (1-4): \n");
+		scanf("%d",&status);
+		switch(status)
+		{
+		case 1:
+			printf("Order Placed\n");
+			break;
+		case 2:
+			printf("Your food is being prepared\n");
+			break;
+		case 3:
+			printf("Out for Delivery\n");
+			break;
+		case 4:
+			printf("Your order has been delivered. Enjoy your meal!\n");
+			break;
+		default:
+			printf("Invalid status. Please enter a number between 1 and 4\n");
+			break;
+		}
+	}
+}
